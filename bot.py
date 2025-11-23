3450af4ee8f13f84c4a580e79420ddc591ff62c2
RadiantValve <easton@heringer-consulting.de>
2025-11-23 23:04:55 +0100

    added backup command to bot, other small changes

--- bot.py
+++ bot.py
@@ -1,6 +1,8 @@
 from dsb import DSBPlanExtractor
 from dotenv import load_dotenv
+from datetime import datetime
 import discord
+import shutil
 import time
 import os
 
@@ -52,7 +54,37 @@ async def on_message(message):
                 if file.endswith('.jpg'):
                     file_list.append(file)
                     await message.channel.send(file=discord.File(f'images/{file}'))
+                    time.sleep(.4)
         print(file_list)
+        await message.channel.send("done")
+    
+    if message.content.startswith("backup"):
+        await message.channel.send("Starting backup...")
+        
+        backup_folder = "backup"
+        if not os.path.exists(backup_folder):
+            os.makedirs(backup_folder)
+        
+        # Move images to the backup folder, organizing by date
+        for file in os.listdir("images"):
+            if file.endswith(".jpg"):
+                # Extract the date from the filename (assuming format like 'subst_8b_2025-11-24.jpg')
+                try:
+                    # Split the filename and extract the date part
+                    date_str = file.split("_")[-1].split(".")[0]  # Extract '2025-11-24'
+                    date_obj = datetime.strptime(date_str, "%Y-%m-%d")
+                    date_folder = os.path.join(backup_folder, date_obj.strftime("%Y-%m-%d"))
+                    
+                    # Create the date folder if it doesn't exist
+                    if not os.path.exists(date_folder):
+                        os.makedirs(date_folder)
+                    
+                    # Move the file to the date folder
+                    shutil.move(os.path.join("images", file), os.path.join(date_folder, file))
+                except ValueError:
+                    await message.channel.send(f"Skipping file with invalid date format: {file}")
+        
+        await message.channel.send("Backup completed!")
     
     else:
         await message.channel.send("Hallo!")
